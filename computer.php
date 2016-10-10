cb3fbdf6e64a52ca290f6b207f612752687b93d1
fujimotosachiko <bb54eight.like@gmail.com>
2016-10-10 11:31:17 +0900

    ・「0÷0」の挙動修正 ・インデント修正 ・文字列が入ってきた場合の処理追加

--- computer.php
+++ computer.php
@@ -1,57 +1,81 @@
 <?php
 if (isset($_GET['operator'])) {
-    switch ($_GET['operator']) {
-      //mb_convert_kana：全角半角変換'n':全角数字→半角数字
-        case '-':
-            $ans = mb_convert_kana($_GET['left'], 'n', 'UTF-8') - mb_convert_kana($_GET['right'], 'n', 'UTF-8');
-            break;
-        case '*':
-            $ans = mb_convert_kana($_GET['left'], 'n', 'UTF-8') * mb_convert_kana($_GET['right'], 'n', 'UTF-8');
-            break;
-        case '/':
-            $ans = mb_convert_kana($_GET['left'], 'n', 'UTF-8') / mb_convert_kana($_GET['right'], 'n', 'UTF-8');
-            break;
-        case '+':
-        default:
-            $ans = mb_convert_kana($_GET['left'], 'n', 'UTF-8') + mb_convert_kana($_GET['right'], 'n', 'UTF-8');
-            break;
-    }
+  //mb_convert_kana：全角半角変換'n':全角数字→半角数字
+$left = mb_convert_kana($_GET['left'], 'n', 'UTF-8');
+  $right = mb_convert_kana($_GET['right'], 'n', 'UTF-8');
+
+  if (is_numeric($left) == ture && is_numeric($right) == ture) {
+      switch ($_GET['operator']) {
+      case '-':
+          $ans = $left - $right;
+          break;
+      case '*':
+          $ans = $left * $right;
+          break;
+      case '/':
+          $ans = $left / $right;
+          if ($ans === false) {
+              $ans = 'error';
+          }
+          break;
+      case '+':
+          $ans = $left + $right;
+          break;
+  }
+  } else {
+      //数字以外の文字が入力された場合
+$moji = '全角または半角の数字を入力してください。';
+  }
 } else {
   //未入力
-    $ans = '計算結果なし';
+  $ans = '計算結果なし';
 }
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8"/>
-      <title>計算機</title>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <link rel="stylesheet" type="text/css" href="css/index.css" media="all">
-    </head>
-    <body>
-      <header>
-        <h1>もっちーの計算機</h1>
-      </header>
-      <main>
-        <form action="computer.php" method="GET">
+
+<head>
+  <meta charset="UTF-8" />
+  <title>計算機</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link rel="stylesheet" type="text/css" href="css/index.css" media="all">
+</head>
+
+<body>
+  <header>
+      <h1>もっちーの計算機</h1>
+  </header>
+  <main>
+      <form action="computer.php" method="GET">
           <input type="text" name="left" required autofocus/>
           <select name="operator">
-            <option value="+" selected>+</option>
-            <option value="-">-</option>
-            <option value="*">×</option>
-            <option value="/">÷</option>
-          </select>
+        <option value="+" selected>+</option>
+        <option value="-">-</option>
+        <option value="*">×</option>
+        <option value="/">÷</option>
+      </select>
           <input type="text" name="right" required/>
           <input type="submit" value="計算する">
-        </form>
-        <p>計算結果：<?php echo $ans; ?></p>
-      </main>
-      <footer>
-        <hr>
-        <small> Copyright (c) 2016 Fujimoto Sachiko, All Rights Reserved.</small>
-        <hr>
-      </footer>
-    </body>
+      </form>
+      <!-- $_GET['left']とr$_GET['right]に数字が入ってきた場合(正常) -->
+      <?php if (is_numeric($left) == ture && is_numeric($right) == ture): ?>
+      <p>
+          <?php echo '計算結果:'.$ans; ?>
+      </p>
+      <!-- $_GET['left']とr$_GET['right]に数字以外の文字が入ってきた場合(エラー) -->
+      <?php endif; ?>
+      <?php if (is_numeric($left) == flase && is_numeric($right) == false): ?>
+      <p>
+          <?php echo $moji; ?>
+      </p>
+      <?php endif; ?>
+  </main>
+  <footer>
+      <hr>
+      <small> Copyright (c) 2016 Fujimoto Sachiko, All Rights Reserved.</small>
+      <hr>
+  </footer>
+</body>
+
 </html>
