45fab75d8434944b668fcf11dad1449a3612cfc6
fujimotosachiko <bb54eight.like@gmail.com>
2016-11-14 14:02:51 +0900

    数字以外の文字がフォームに入った場合のバグを修正

--- computer.php
+++ computer.php
@@ -4,7 +4,6 @@ if (isset($_GET['operator'])) {
     //mb_convert_kana：全角半角変換'n':全角数字→半角数字
     $left = mb_convert_kana($_GET['left'], 'n', 'UTF-8');
     $right = mb_convert_kana($_GET['right'], 'n', 'UTF-8');
-
     // 変数が数字または数値形式の文字列であるかを調べる
     if (is_numeric($left) == true && is_numeric($right) == true) {
         switch ($_GET['operator']) {
@@ -16,7 +15,7 @@ if (isset($_GET['operator'])) {
           break;
       case '/':
           $ans = $left / $right;
-          // $rightに0が入った場合(値はfalseになる):error表示
+          // $rightに0が入った場合(値はfalseになる):error
           if ($ans === false) {
               $ans = 'error';
           }
@@ -24,7 +23,7 @@ if (isset($_GET['operator'])) {
       case '+':
           $ans = $left + $right;
           break;
-  }
+        }
     } else {
         //数字以外の文字が入力された場合
         $moji = '全角または半角の数字を入力してください。';
@@ -62,13 +61,13 @@ if (isset($_GET['operator'])) {
           <input type="submit" value="計算する">
       </form>
       <!-- $_GET['left']とr$_GET['right]に数字が入ってきた場合(正常) -->
-      <?php if (is_numeric($left) == ture && is_numeric($right) == ture): ?>
+      <?php if (is_numeric($left) == true && is_numeric($right) == true): ?>
       <p>
           <?php echo '計算結果:'.$ans; ?>
       </p>
-      <!-- $_GET['left']と$_GET['right]に数字以外の文字が入ってきた場合(エラー) -->
+      <!-- $_GET['left']とr$_GET['right]に数字以外の文字が入ってきた場合(エラー) -->
       <?php endif; ?>
-      <?php if (is_numeric($left) == flase && is_numeric($right) == false): ?>
+      <?php if (is_numeric($left) == false || is_numeric($right) == false): ?>
       <p>
           <?php echo $moji; ?>
       </p>
